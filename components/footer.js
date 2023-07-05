28a6f37c1e8ed130c7f9b3ebeb0d221da255a94a
pankajsbisht143 <pankajsbisht143@gmail.com>
2023-07-05 22:02:55 +0530

    done

---
+++ components/footer.js
@@ -0,0 +1,54 @@
+let footer=()=>{
+    return `<div id="footImg">
+    <img src="./images/png.png" alt="">
+ </div>
+ <div id="footer">
+  <div id="foot">
+   <div>
+     <h2>All Medicine</h2>
+     <div>
+     <h3>Health&Care</h3>
+     <p>Beauty Care</p>
+ </div>
+   </div>
+   <div>
+     <h2>Doctors</h2>
+     <div>
+     <h3>Physiotherapy</h3>
+     <p>Heart Specialist</p>
+ </div>
+   </div>
+   <div>
+     <h2>Hospital</h2>
+     <div>
+     <h3>Government listed</h3>
+     <p>Top private</p>
+ </div>
+   </div>
+   <div>
+     <h2>About Us</h2>
+     <div>
+     <h3>Contact Us</h3>
+     
+ </div>
+   </div>
+   <div></div>
+  </div>
+  <!-- <div id="hr">
+
+  </div> -->
+ <hr>
+  <div id="footme">
+
+     <div>
+      <p>@YourPharmacy 2023 All rights reserved.</p>
+     </div>
+     <div>
+         <img src="./images/appleplay.png" alt="">
+         <img src="./images/googleplay.png" alt="">
+     </div>
+  </div>
+
+ </div>`
+}
+export default footer;
