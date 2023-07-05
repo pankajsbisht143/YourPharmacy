e44c21e896a9c9cedee18e0aed2b14fdd4cf2f1d
Ashutosh Kumawat <aurecordsofficial@gmail.com>
2023-07-05 12:28:05 +0530

    working on css

--- script/address.js
+++ script/address.js
@@ -1,11 +1,13 @@
-let address  = document.getElementById("address_form");
+let address  = document.getElementById("submit");
 
-address.addEventListener("submit",function(){
-    displayAddress();
+address.addEventListener("click",function(){
+    submitAddress();
 })
 
 let address_array = JSON.parse(localStorage.getItem("user_address")) || [];
- function displayAddress(){
+
+displayAddress(address_array);
+ function submitAddress(){
     event.preventDefault();
     let pincode = document.getElementById("pincode").value;
     let city = document.getElementById("city").value;
@@ -14,14 +16,90 @@ let address_array = JSON.parse(localStorage.getItem("user_address")) || [];
     let lastName = document.getElementById("lName").value;
     let address = document.getElementById("address").value;
     let number = document.getElementById("number").value;
-    
-    address_array.push(pincode,city,state,firstName,lastName,address,number);
+    let locality = document.getElementById("locality").value;
+
+    if(pincode == "" ||
+       city == ""  ||
+       state == ""  ||
+       firstName == ""  ||
+       lastName == ""  ||
+       address == ""  ||
+       number == ""  ||
+       locality == ""
+       ){
+        alert("Please Fill All The Required Fields");
+        return;
+       }
+
+    let address_Obj = {
+        firstName : firstName,
+        lastName : lastName,
+        number : number,
+        pincode : pincode,
+        address :address,
+        city : city,
+        state : state,
+        locality : locality    
+    }
+    console.log(address_Obj)
+    address_array.push(address_Obj);
 
     localStorage.setItem("user_address",JSON.stringify(address_array));
 
-    let container_address = document.createElement("div");
-    container_address.append(address_array);
+    document.getElementById("pincode").value = "";
+    document.getElementById("city").value = "";
+    document.getElementById("state").value = "";
+    document.getElementById("fName").value = "";
+    document.getElementById("lName").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("number").value = "";
+    document.getElementById("locality").value = "";
 
-    document.querySelector("body").append(container_address);
-    
+}
+
+function displayAddress(arr){
+document.getElementById("add_here").innerText = "";
+
+arr.map(function(elem,idx){
+    var Main_div = document.createElement("div");
+    Main_div.setAttribute("id","add_div_here");
+
+    var userName = document.createElement("h3");
+    userName.textContent = elem.firstName+" "+elem.lastName;
+
+    var userADD = document.createElement("p");
+    userADD.textContent = elem.address+","+elem.city+","+elem.state+","+elem.pincode;
+
+    var num = document.createElement("p");
+    num.textContent = "Mobile :"+" "+elem.number;
+
+    var remove = document.createElement("button");
+    remove.textContent = "REMOVE";
+    remove.addEventListener("click",function(){
+        deleteFun(elem,idx);
+    });
+
+    var edit = document.createElement("button");
+    edit.textContent = "EDIT";
+
+    Main_div.append(userName,userADD,num,remove,edit);
+    document.getElementById("add_here").append(Main_div);
+
+    // <-------- display none property ------->
+
+    var magic = document.getElementById("address_mainDIV");
+
+    magic.style.display = "None";
+});
+    var top = document.getElementById("top");
+    top.style.border = "none";
+
+}
+
+function deleteFun(elem,idx){
+    address_array.splice(idx,1);
+    localStorage.setItem("user_address",JSON.stringify(address_array));
+    displayAddress(address_array);
+    var magic = document.getElementById("address_mainDIV");
+    magic.style.display = "visible";
 }
